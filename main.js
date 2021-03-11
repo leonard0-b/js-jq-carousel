8e8c491691dd00cc913e46a2af5eb1c2281e92b1
Leonardo <leonardo.bongianni@gmail.com>
2021-03-11 17:04:35 +0100

    Esercizio Completo

--- main.js
+++ main.js
@@ -1,35 +1,44 @@
 $(document).ready(function(){
   slideRight();
   slideLeft();
-  lastFirst();
 })
 
 
 
 
 // funzioni
+// nomino la funzione slideRight🐱‍👤
 function slideRight (){
+  // creo una variabile puntando sulla freccia destra🐱‍👤
   var right = $(".fa-angle-right");
+  // richiamo la funzione .click
   right.click(function() {
+    // creo la var img puntando la prima img con classe active🐱‍👤
     var img = $(".images > .active");
+    // al click rimuovo la classe active per darla al suo "next"🐱‍👤
     img.removeClass("active").next().addClass("active");
-  })
-}
-
-function slideLeft (){
-  var left = $(".fa-angle-left");
-  left.click(function() {
-    var img = $(".images > .active");
-    img.removeClass("active").prev().addClass("active");
-  })
-}
-
-function lastFirst (){
-  var right = $(".fa-angle-right");
-  right.click(function(){
-    var img = $(".images > .active");
+    // se l'immagine ha classe "last" allora rimuovo la classe "active" e la aggiungo all'img con classe "first"🐱‍👤
     if (img.hasClass("last")) {
-      img.next().find(".first").addClass("active");
+      img.removeClass("active");
+      $("img.first").addClass("active");
+    }
+  })
+}
+
+// nomino la funzione slideLeft🐱‍👤
+function slideLeft (){
+  // creo la variabile left puntando la freccia sinistra🐱‍👤
+  var left = $(".fa-angle-left");
+  // al click sulla freccia sinistra🐱‍👤
+  left.click(function() {
+    // creo la variabile img puntando l'img con classe active🐱‍👤
+    var img = $(".images > .active");
+    // al click rimuovo la classe active e la aggiungo al suo precedente immediato🐱‍👤
+    img.removeClass("active").prev().addClass("active");
+    // se ha classe "first" allora rimuovo la classe "active" e la aggiungo all'img con classe "last"🐱‍👤
+    if (img.hasClass("first")) {
+      img.removeClass("active");
+      $("img.last").addClass("active");
     }
   })
 }
